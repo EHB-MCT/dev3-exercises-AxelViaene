146afa1b0c195706daf5586de6d47747b2eb4b3b
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2021-10-14 17:04:06 +0200

    Challenge2

--- session2/Adventure_Game/src/main/kotlin/main.kt
+++ session2/Adventure_Game/src/main/kotlin/main.kt
@@ -9,11 +9,34 @@ fun challengeOne() {
     var userAnswer = readLine()
     if (answers.contains(userAnswer)) {
         println("You may succeed")
+        challengeTwo()
+
     }
 else{
     println("Try again")
     }
 }
 
+fun challengeTwo(){
+    println("I roll a 6-sided die. Guess high or low.")
+    val randomNumber = (1..6).random()
+    val guessNumber = readLine()
+var acceptedAnswers = setOf<Int>()
 
+    if (guessNumber == "low"){
+         acceptedAnswers = setOf<Int>(1, 2, 3)
+    }
+
+    if (guessNumber == "high"){
+        acceptedAnswers = setOf<Int>(4, 5, 6)
+    }
+
+
+    if (acceptedAnswers.contains(randomNumber)) {
+       println("Correct")
+    }
+    else {
+    println("Game Over")
+    }
+}
 
