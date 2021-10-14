021ffd1b24db8334fd4e0b69e87a81bb5bd927eb
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2021-10-14 17:30:46 +0200

    WIP challenge4

--- session2/Adventure_Game/src/main/kotlin/main.kt
+++ session2/Adventure_Game/src/main/kotlin/main.kt
@@ -4,8 +4,8 @@ fun main() {
 }
 
 fun challengeOne() {
-    println("Give a name of a city in Middle Earth.")
-    val answers = setOf("Edoras", "Minas Tirith", "Lothlien", "Bree")
+    println("Before you leave on your journey, let's make sure you are knowledgable enough about the world. Give a name of a city in Middle Earth.")
+    val answers = setOf("Edoras", "Minas Tirith", "Lothlien", "Bree", "Rivendell", "Axel")
     var userAnswer = readLine()
     if (answers.contains(userAnswer)) {
         println("You may succeed")
@@ -21,6 +21,7 @@ fun challengeTwo(){
     println("I roll a 6-sided die. Guess high or low.")
     val randomNumber = (1..6).random()
     val guessNumber = readLine()
+    println(randomNumber)
 var acceptedAnswers = setOf<Int>()
 
     if (guessNumber == "low"){
@@ -46,7 +47,7 @@ fun challengeThree(){
     val answers = setOf("sword", "bow", "staff")
     var userAnswer = readLine()
     if (userAnswer == "sword"){
-        println("game over")
+        println("Sword are no use here!")
     }
     if (userAnswer == "bow"){
         println("You are driven back but manage to retreat.")
@@ -54,6 +55,15 @@ fun challengeThree(){
     }
     if (userAnswer == "staff"){
         println("You shall not pass! Balrog defeated")
-       
+        challengeFour()
     }
+}
+
+fun challengeFour(){
+    println("Guess a number between 1 and 100")
+    val randomNumber = (1..100).random()
+    val randomNumberTest = 51
+    var userAnswer = readLine()
+
+
 }
